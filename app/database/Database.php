5b8c56311a8b58a18133e6c13eea7c0729294ada
Kodokunaa <amaionigiri04@gmail.com>
2026-09-16 09:18:12 +0800

    Move Aiven database SSL support to app layer

--- app/database/Database.php
+++ app/database/Database.php
@@ -268,6 +268,13 @@ class Database {
             PDO::ATTR_EMULATE_PREPARES   => false,
         );
 
+        // Application-specific Aiven TLS support. Local databases can leave DB_SSL_CA empty.
+        if ($driver === 'mysql' && !empty($database_config['ssl_ca'])
+            && defined('PDO::MYSQL_ATTR_SSL_CA') && is_readable($database_config['ssl_ca'])) {
+            $options[PDO::MYSQL_ATTR_SSL_CA] = $database_config['ssl_ca'];
+            $options[PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT] = true;
+        }
+
         try {
             $this->db = new PDO($dsn, $username, $password, $options);
             $this->driver = $this->db->getAttribute(PDO::ATTR_DRIVER_NAME);
